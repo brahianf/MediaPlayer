41a0b38f2171cceb0dfd7e04a861d7c0c929c951
brahianf <brahianf@unicauca.edu.co>
2020-07-15 11:47:08 -0500

    Primer Plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -14,4 +14,18 @@ MediaPlayer.prototype.play = function () {
     }   
 }
 
+MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(plugin => {
+        plugin.run(this)
+    })
+}
+
+MediaPlayer.prototype.mute = function () {
+    this.media.muted= true;
+}
+
+MediaPlayer.prototype.unmute = function () {
+    this.media.muted= false;
+}
+
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,11 @@
-// import MediaPlayer from './MediaPlayer.js'
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from '../plugins/AutoPlay.js'
 
-// Clase con objeto de configuración  
-function MediaPlayer(config){
-    // Leer cual es el elemento de config que representa el media
-    this.media = config.el
-}
-
-// Método play añadido al prototype
-MediaPlayer.prototype.play = function () {
-    if(this.media.paused){
-        this.media.play();
-    } else {
-        this.media.pause();
-    }   
-}
 
 // Selector o varios
 const video = document.querySelector("video");
 const button = document.querySelector("button");
-const player = new MediaPlayer({el: video});
+const player = new MediaPlayer({el: video, plugins: [new AutoPlay()]});
 
 // Play(), todos los elementos del DOM tienen API
 // htmlmediaelement => propiedades eventos y metodos
