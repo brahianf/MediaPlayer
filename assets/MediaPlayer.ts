e4efcee7856a3632e22250d25ccc8f5447755a36
brahianf <brahianf@unicauca.edu.co>
2020-07-19 01:08:45 -0500

    Implementacion de plugin de Ads en Pantalla

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -3,11 +3,21 @@
 class MediaPlayer {
     media: HTMLMediaElement;
     plugins: Array<any>;
+    container: HTMLElement;
+
     constructor(config) {
         // Leer cual es el elemento de config que representa el media
         this.media = config.el;
         this.plugins = config.plugins || [];
+        this.initPlayer();
         this.initPlugins();
+    }
+
+    initPlayer(){
+        this.container = document.createElement('div')
+        this.container.style.position = 'relative';
+        this.media.parentNode.insertBefore(this.container, this.media);
+        this.container.appendChild(this.media); 
     }
     private initPlugins() {
 
